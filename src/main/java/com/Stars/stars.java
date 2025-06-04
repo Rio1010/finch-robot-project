b1d88370bd725f2dccdf5ffd7528b401b8499ba1
Rio <Pahmadi0004@mymail.lausd.net>
2025-06-04 10:35:29 -0700

    finished project maze

--- src/main/java/com/Stars/stars.java
+++ src/main/java/com/Stars/stars.java
@@ -4,15 +4,55 @@ import com.birdbrain.Finch;
 
 public class FinchDemo {
     public static void main(String[] args) {
-        Finch myFinch = new Finch();
+        Finch star = new Finch();
 
-        myFinch.playNote(60,0.5);
+        stars.setMove("F", 50, 50);
+        stars.setMove("F", 30, 50);
+        stars.setMove("F", 20, 50);
+        stars.setMove("F", 5, 20);
+        stars.stop();
+        stars.setTurn("L", 90, 50);
 
-        Myfinch.setMove("F", 10,50);
 
-        
+        stars.stop();
+        stars.setMove("F", 40, 40);
+        stars.stop();
+        stars.setTurn("L", 90, 50);
+        stars.setMove("F", 30, 50);
 
-        myFinch.stopAll();
-        myFinch.disconnect();
+
+        stars.stop();
+
+
+        stars.setTurn("R", 90, 50);
+        stars.setMove("F", 30, 50);
+        stars.setMove("F", 15, 50);
+
+
+        stars.setTurn("R", 90, 50);
+        stars.setMove("F", 100, 50);
+
+
+        stars.stop();
+
+
+        stars.setMove("F", 15, 50);
+
+
+        stars.setTurn("R", 90, 50);
+        // stars.setTurn("R", 10, 50);
+
+
+        stars.setMove("F", 15, 50);
+        stars.setTurn("L", 90, 50);
+        stars.setMove("F", 15, 50);
+        stars.setMove("F", 10, 50);
+
+
+        stars.setTurn("L", 90, 50);
+        stars.setMove("F", 15, 90);
+
+        // star.stopAll();
+        // star.disconnect();
     }
 }
